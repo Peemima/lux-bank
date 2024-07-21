6a45b78352804b7db98851cc39592c168fc2e665
Peemima <96958647+Peemima@users.noreply.github.com>
2024-07-21 16:11:26 -0400

    Update loading-screen.js

--- assets/loading-screen.js
+++ assets/loading-screen.js
@@ -7,7 +7,7 @@ function hideLoadingScreen() {
   setTimeout(() => { 
     // Set the display property to 'none'
     document.getElementById("loading-overlay").style.display = "none"; 
-  }, 100);
+  }, 400);
 }
 
 // Add loading overlay to the page
